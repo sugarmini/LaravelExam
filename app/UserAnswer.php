ff3fe981428bc9d74a41374174e41ef58e010ae3
sugus <jxffight@126.com>
2017-10-26 16:34:55 +0800

    记录答案

---
+++ app/UserAnswer.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAnswer extends Model{
+    protected $table = 'user_answer';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+}
